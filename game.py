a5036df3e765a8801b5d63bc8ec183169ab6551b
Thyago Euclides <flooddk@gmail.com>
2025-10-31 20:17:57 -0400

    Implements random damage calculation
    Introduces variability to attack damage for both regular and special abilities. Previously, damage was a fixed value based on level.
    
    Now, damage is calculated within a random range, enhancing gameplay by making combat outcomes more dynamic and less predictable.

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
+import random
+# Personagem:classe mãe
+# Herói: controlado pelo usuário
+# Inimigo: adversário do usuário
+
+
 class Character:
     def __init__(self, name, life, level):
         self.__name = name
@@ -22,7 +28,8 @@ class Character:
             self.__life = 0
 
     def attack(self, target):
-        damage = self.__level * 2
+        damage = random.randint(self.get_level() * 2,
+                                self.get_level() * 4)  # baseado no nível
         target.receive_attack(damage)
         print(
             f"{self.get_name()} atacou {target.get_name()} e causou {damage} de dano!")
@@ -40,7 +47,8 @@ class Hero(Character):
         return f"{super().show_details()}\nHabilidade: {self.get_skill()}\n"
 
     def special_attack(self, target):
-        damage = self.get_level() * 5
+        damage = damage = random.randint(self.get_level() * 5,
+                                         self.get_level() * 8)  # dano aumentado
         target.receive_attack(damage)
         print(f"{self.get_name()} usou a habilidade especial {self.get_skill()} em {target.get_name()} e causou {damage} de dano!")
 
